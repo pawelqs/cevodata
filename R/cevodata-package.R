79a3bfbd62e0371c5f3af8940a6c0b872744e5be
Paweł <kpawel2210@gmail.com>
2024-01-09 14:01:15 +0100

    Core files set up

---
+++ R/cevodata-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import dplyr
+#' @import forcats
+#' @import ggplot2
+#' @import purrr
+#' @import readr
+#' @import stringr
+#' @import tibble
+#' @import tidyr
+#' @importFrom rlang :=
+#' @importFrom rlang .data
+#' @importFrom rlang .env
+#' @importFrom tidyselect where
+## usethis namespace: end
+NULL
