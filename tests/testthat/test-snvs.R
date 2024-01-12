98cee2e4988eaab8ba1dcfa43833c4f0066e149b
Paweł <kpawel2210@gmail.com>
2024-01-12 08:31:04 +0100

    Update test-snvs.R

--- tests/testthat/test-snvs.R
+++ tests/testthat/test-snvs.R
@@ -4,3 +4,42 @@ test_that("as_cevo_snvs() works", {
   res <- as_cevo_snvs(snvs)
   expect_s3_class(res, c("cevo_snvs", "tbl_df", "tbl", "data.frame"))
 })
+
+
+test_that("SNVs_CNAs() works", {
+  snvs <- tribble(
+    ~sample_id, ~chrom, ~pos, ~VAF,
+    "S1",       "chr1", 100,  0.1,
+    "S1",       "chr1", 200,  0.1,
+    "S1",       "chr1", 300,  0.1,
+    "S1",       "chr1", 400,  0.1,
+    "S1",       "chr2", 100,  0.1,
+    "S2",       "chr1", 100,  0.1,
+    "S2",       "chr1", 300,  0.1
+  )
+  snvs$ref <- "C"
+  snvs$alt <- "T"
+  cnas <- tribble(
+    ~sample_id, ~chrom, ~start, ~end, ~total_cn, ~minor_cn, ~normal_cn,
+    "S1",       "chr1", 1,      250,  3,         1,         2,
+    "S1",       "chr1", 350,    450,  4,         1,         2,
+    "S1",       "chr2", 1,      250,  2,         1,         2,
+    "S2",       "chr1", 1,      250,  3,         1,         2
+  )
+  cd <- init_cevodata("test", snvs = snvs, cnas = cnas)
+  expected <- tribble(
+    ~sample_id, ~chrom, ~pos, ~VAF, ~total_cn, ~minor_cn, ~normal_cn,
+    "S1",       "chr1", 100,  0.1,  3,         1,         2,
+    "S1",       "chr1", 200,  0.1,  3,         1,         2,
+    "S1",       "chr1", 300,  0.1,  NA_real_,  NA_real_,  NA_real_,
+    "S1",       "chr1", 400,  0.1,  4,         1,         2,
+    "S1",       "chr2", 100,  0.1,  2,         1,         2,
+    "S2",       "chr1", 100,  0.1,  3,         1,         2,
+    "S2",       "chr1", 300,  0.1,  NA_real_,  NA_real_,  NA_real_
+  )
+  res <- SNVs_CNAs(cd)
+  # sample_data <- tibble(sample_id = c("S1", "S2"), sex = c())
+  expect_identical(res$total_cn, expected$total_cn)
+  expect_identical(res$minor_cn, expected$minor_cn)
+})
+
