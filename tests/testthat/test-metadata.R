26dfbbe4a5168d91916a07a8b6afff95534d5a69
Paweł <kpawel2210@gmail.com>
2024-01-29 16:16:37 +0100

    get_patients_data() exported

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -9,3 +9,27 @@ test_that("use_purity() throws error if nonexisting purity column is requested",
   object <- test_data
   expect_error(use_purity(object, "purity_2"))
 })
+
+
+test_that("get_patients_data() works", {
+  sample_data <- tibble(
+    sample_id = c("sampleA1", "sampleA2", "sampleB1", "sampleB2"),
+    purity = c(0.5, 0.6, 0.5, 0.5)
+  )
+  samples_to_patients <- tibble(
+    sample_id = c("sampleA1", "sampleA2", "sampleB1", "sampleB2"),
+    patient_id = c("patientA", "patientA", "patientB", "patientB")
+  )
+  patients_data <- tibble(
+    patient_id = c("patientA", "patientB"),
+    sex = c("male", "female")
+  )
+
+  object <- init_cevodata() |>
+    add_metadata(sample_data) |>
+    add_metadata(samples_to_patients) |>
+    add_metadata(patients_data)
+
+  res <- get_patients_data(object)
+  expect_equal(res, patients_data)
+})
